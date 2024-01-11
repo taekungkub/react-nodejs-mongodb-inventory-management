910a38b4c98c2e02c0c078f04073c584b88feff5
Kieattisak Suparit <tokimekiyume@hotmail.com>
2024-01-11 10:44:29 +0700

    fix: api

--- frontend/src/services/Api.ts
+++ frontend/src/services/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const ApiOne = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://localhost:4000/api",
   headers: {
     "Content-Type": "application/json",
   },
