11ef1bd95b2949f062a2bac15d134f7b64be7910
Kieattisak Suparit <tokimekiyume@hotmail.com>
2024-01-22 23:19:56 +0700

    add print

--- frontend/src/views/OrderDetail/index.tsx
+++ frontend/src/views/OrderDetail/index.tsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom"
 import useOrder from "../../hooks/use-order"
 import "./index.css"
-import { Button, Flex } from "@mantine/core"
+import { Button, Flex, Title } from "@mantine/core"
 export default function OrderDetailPage() {
   let params = useParams()
 
@@ -11,16 +11,20 @@ export default function OrderDetailPage() {
   return (
     <div>
       <Flex justify={"flex-end"}>
-        <Button>Print</Button>
+        <Button className="hide-on-print" onClick={() => window.print()}>
+          Print
+        </Button>
       </Flex>
+      <Title order={4}>Bill: {data?._id}</Title>
+
+      <Title order={4}>ผู้สั่ง: {data?.userId.username}</Title>
       <div className="table-responsive">
         <table className="table">
           <thead>
             <tr>
-              <th className="text-left" style={{ width: "70px" }}>
-                No.
-              </th>
+              <th className="text-left">No.</th>
               <th className="text-left">Item</th>
+              <th className="text-left"> Qty</th>
               <th className="text-right">Price</th>
             </tr>
           </thead>
@@ -28,27 +32,25 @@ export default function OrderDetailPage() {
             {data?.items.map((v, i) => (
               <tr key={i}>
                 <td>{i + 1}</td>
-                <td>
-                  {v.productId.title} x {v.qty}
-                </td>
-                <td className="text-right">${v.productId.price}</td>
+                <td>{v.productId.title}</td>
+                <td>{v.qty}</td>
+                <td className="text-right">${v.productId.price * v.qty}</td>
               </tr>
             ))}
-
             <tr>
-              <td colSpan={2} className="text-right">
+              <td colSpan={3} className="text-right">
                 <strong>Sub Total</strong>
               </td>
               <td className="text-right">$1397.00</td>
             </tr>
             <tr className="border-0">
-              <td colSpan={2} className="text-right">
+              <td colSpan={3} className="text-right">
                 <strong>Shipping</strong>
               </td>
               <td className="text-right">$13.00</td>
             </tr>
             <tr className="border-0">
-              <td colSpan={2} className="text-right">
+              <td colSpan={3} className="text-right">
                 <strong>Total</strong>
               </td>
               <td className="text-right">
